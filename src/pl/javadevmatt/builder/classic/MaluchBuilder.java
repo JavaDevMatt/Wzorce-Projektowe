50bbcafd395595183fb27ad3545121ab7ff9198f
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-03 21:17:05 +0200

    added MaluchBuilder

--- src/pl/javadevmatt/builder/classic/MaluchBuilder.java
+++ src/pl/javadevmatt/builder/classic/MaluchBuilder.java
@@ -4,29 +4,29 @@ import pl.javadevmatt.builder.classic.entities.Car;
 import pl.javadevmatt.builder.classic.entities.Engine;
 import pl.javadevmatt.builder.classic.entities.Tires;
 
-public class RaceCarBuilder implements CarBuilder{
+public class MaluchBuilder implements CarBuilder{
 	
 	private Car car;
 	
-	public RaceCarBuilder(){
+	public MaluchBuilder(){
 		this.car = new Car();
-	}
-	
-	@Override
-	public void buildEngine() {
-		Engine engine = new Engine();
-		engine.setType("v8");
-		
-		car.setEngine(engine);
 	}
 
 	@Override
 	public void buildTires() {
 		Tires tire = new Tires();
-		tire.setDurability(50);
-		tire.setType("Slicks");
+		tire.setDurability(100);
+		tire.setType("Maluch Tires");
 		
 		car.setTires(tire);
+	}
+
+	@Override
+	public void buildEngine() {
+		Engine engine = new Engine();
+		engine.setType("Maluch Engine");
+		
+		car.setEngine(engine);
 	}
 
 	@Override
