c122b27e106ac80174d9409cf6c377895197e164
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-20 11:43:42 +0200

    update of the YTUser class

--- src/pl/javadevmatt/observer/YTUser.java
+++ src/pl/javadevmatt/observer/YTUser.java
@@ -5,14 +5,17 @@ import pl.javadevmatt.observer.interfaces.Observer;
 public class YTUser implements Observer{
 	
 	private String name;
+	private int videosToWatch;
 	
 	public YTUser(String name) {
 		this.name = name;
+		videosToWatch = 0;
 	}
 	
 	@Override
 	public void update() {
-		System.out.println("Hey " + name + "! There is a new video!");
+		videosToWatch++;
+		System.out.println("Hey " + name + "! There is a new video! You have " + videosToWatch + " videos to watch.");
 	}
 
 }
